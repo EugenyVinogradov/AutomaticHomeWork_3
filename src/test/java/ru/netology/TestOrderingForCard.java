55a0efe5f94eeeb2f83b9589d6681c6e57ff8720
EugenyVinogradov <vinogradov_e_v@rambler.ru>
2022-07-21 12:58:57 +0300

    All validation tests with BUILD FAILED tests

--- src/test/java/ru/netology/TestOrderingForCard.java
+++ src/test/java/ru/netology/TestOrderingForCard.java
@@ -151,24 +151,24 @@ public class TestOrderingForCard {
         form.$("[type=button]").click();
         $(".input_invalid [name=agreement]").shouldBe();
     }
-    //    @Test
-//    void shouldValidateFormByNameContainsOneHyphen() {
-//        open("http://localhost:9999");
-//        SelenideElement form = $("form");
-//        form.$("[data-test-id=name] input").setValue("-");
-//        form.$("[data-test-id=phone] input").setValue("+79270000000");
-//        form.$("[data-test-id=agreement]").click();
-//        form.$("[type=button]").click();
-//        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-//    }
-//    @Test
-//    void shouldValidateFormByNameContainsOnlyHyphens() {
-//        open("http://localhost:9999");
-//        SelenideElement form = $("form");
-//        form.$("[data-test-id=name] input").setValue("------");
-//        form.$("[data-test-id=phone] input").setValue("+79270000000");
-//        form.$("[data-test-id=agreement]").click();
-//        form.$("[type=button]").click();
-//        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-//    }
+        @Test
+    void shouldValidateFormByNameContainsOneHyphen() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("-");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+    @Test
+    void shouldValidateFormByNameContainsOnlyHyphens() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("------");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
 }
