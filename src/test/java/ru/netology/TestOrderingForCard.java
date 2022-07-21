9a0e3e9bd02ea69c0728463410ae59c4f0cd8a1b
EugenyVinogradov <vinogradov_e_v@rambler.ru>
2022-07-21 12:57:46 +0300

    Create validation tests without BUILD FAILED tests

--- src/test/java/ru/netology/TestOrderingForCard.java
+++ src/test/java/ru/netology/TestOrderingForCard.java
@@ -1,6 +1,8 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
@@ -8,44 +10,165 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestOrderingForCard {
-        @Test
-        void shouldSubmitRequest() {
-            open("http://localhost:9999");
-            SelenideElement form = $("form");
-            form.$("[data-test-id=name] input").setValue("Василий");
-            form.$("[data-test-id=phone] input").setValue("+79270000000");
-            form.$("[data-test-id=agreement]").click();
-            form.$("[type=button]").click();
-            $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
-        }
-        @Test
-        void shouldSubmitRequestByNameContainsSpace() {
-            open("http://localhost:9999");
-            SelenideElement form = $("form");
-            form.$("[data-test-id=name] input").setValue("Василий Алибабаевич");
-            form.$("[data-test-id=phone] input").setValue("+79270000000");
-            form.$("[data-test-id=agreement]").click();
-            form.$("[type=button]").click();
-            $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
-        }
-        @Test
-        void shouldSubmitRequestByNameContainsSpaceAndHyphen() {
-            open("http://localhost:9999");
-            SelenideElement form = $("form");
-            form.$("[data-test-id=name] input").setValue("Василий Алибабаевич Алибабаев-Задунайский");
-            form.$("[data-test-id=phone] input").setValue("+79270000000");
-            form.$("[data-test-id=agreement]").click();
-            form.$("[type=button]").click();
-            $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
-        }
-        @Test
-        void shouldSubmitRequestByPhoneContainsAllZero() {
-            open("http://localhost:9999");
-            SelenideElement form = $("form");
-            form.$("[data-test-id=name] input").setValue("Василий Алибабаевич Алибабаев-Задунайский");
-            form.$("[data-test-id=phone] input").setValue("+00000000000");
-            form.$("[data-test-id=agreement]").click();
-            form.$("[type=button]").click();
-            $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
-        }
+    @Test
+    void shouldSubmitRequest() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Василий");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldSubmitRequestByNameContainsSpace() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Василий Алибабаевич");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldSubmitRequestByNameContainsSpaceAndHyphen() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Василий Алибабаевич Алибабаев-Задунайский");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldSubmitRequestByPhoneContainsAllZero() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Василий Алибабаевич Алибабаев-Задунайский");
+        form.$("[data-test-id=phone] input").setValue("+00000000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldValidateFormByNameContainsLatin() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Василий Алибабаевич Z");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldValidateFormByNameContainsSpecialSimbols() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Василий Алибабаевич.");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldValidateFormByNameIsEmpty() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $("[data-test-id=name] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldValidateFormByNameContainOnlySpaces() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("           ");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $("[data-test-id=name] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldValidateFormByPhoneNotContainFirstPlus() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Василий Алибабаевич");
+        form.$("[data-test-id=phone] input").setValue("79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldValidateFormByPhoneNotContainPlusNotFirst() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Василий Алибабаевич");
+        form.$("[data-test-id=phone] input").setValue("79270000000+");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldValidateFormByPhoneNotContainFewerThanElevenDigits() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Василий Алибабаевич");
+        form.$("[data-test-id=phone] input").setValue("+7927000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldValidateFormByPhoneNotContainMoreThanElevenDigits() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Василий Алибабаевич");
+        form.$("[data-test-id=phone] input").setValue("+792700000001");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldValidateFormByNotIncludedCheckbox() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=name] input").setValue("Василий Алибабаевич");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[type=button]").click();
+        $(".input_invalid [name=agreement]").shouldBe();
+    }
+    //    @Test
+//    void shouldValidateFormByNameContainsOneHyphen() {
+//        open("http://localhost:9999");
+//        SelenideElement form = $("form");
+//        form.$("[data-test-id=name] input").setValue("-");
+//        form.$("[data-test-id=phone] input").setValue("+79270000000");
+//        form.$("[data-test-id=agreement]").click();
+//        form.$("[type=button]").click();
+//        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+//    }
+//    @Test
+//    void shouldValidateFormByNameContainsOnlyHyphens() {
+//        open("http://localhost:9999");
+//        SelenideElement form = $("form");
+//        form.$("[data-test-id=name] input").setValue("------");
+//        form.$("[data-test-id=phone] input").setValue("+79270000000");
+//        form.$("[data-test-id=agreement]").click();
+//        form.$("[type=button]").click();
+//        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+//    }
 }
